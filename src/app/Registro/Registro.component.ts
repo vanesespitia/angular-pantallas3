951062463b17894c8d6fe984a16535d79b290290
vanesespitia <vaneespitiagv@gmail.com>
2019-11-20 12:33:07 -0600

    iniciar,login,registro-agregados

---
+++ src/app/Registro/Registro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+selector: 'Registro',
+template: './Registro.component.html',
+styleUrls: [ './Registro.css' ]
+
+})
+
+export class RegistroComponent  {
+  name = 'Registro';
+}
